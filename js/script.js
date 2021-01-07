6dabdb5fc33ab3110a993eb49bda2100897587d9
lucio rendine <luciorendine69@gmail.com>
2021-01-07 16:44:52 +0100

    second push

--- js/script.js
+++ js/script.js
@@ -1,13 +1,32 @@
-var oddOrEven = prompt('Pari o Dispari?')
-var number
+
+var min = 1;
+var max = 5;
+
+var oddOrEven = prompt('Pari o Dispari?');
+var numberUser;
 if (oddOrEven === 'Pari' || oddOrEven === 'Dispari') {
-  number = prompt('Scegli un numero da 1 a 5')
+  numberUser = parseInt(prompt('Scegli un numero da 1 a 5'));
 } else {
-  alert("Segui le istruzioni")
+  alert("Segui le istruzioni");
 }
 
-if (number === 1 || number === 2 || number === 3 || number === 4 || number === 5) {
-
+if (numberUser === 1 || numberUser === 2 || numberUser === 3 || numberUser === 4 || numberUser === 5) {
+  numberPc = Math.floor(Math.random() * (max - min) ) + min
+  sum = numberPc + numberUser
+  console.log('Il risultato è: ' + sum);
 } else {
-  alert('Segui le istruzioni')
+  alert('Segui le istruzioni');
 }
+
+// if (sum % 2 === 0) {
+//   sum = 'Pari'
+// } else {
+//   sum = 'Dispari'
+// }
+//
+// if (sum = 'Pari' || sum = 'Dispari') {
+//   oddOrEven === sum
+//   alert('Hai vinto')
+// } else {
+//   alert('Hai Perso')
+// }
