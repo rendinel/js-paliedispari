e2d8d4da5a45d8667067a1d831061e4172b06cb7
lucio rendine <luciorendine69@gmail.com>
2021-01-07 15:57:38 +0100

    first push

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var oddOrEven = prompt('Pari o Dispari?')
+var number
+if (oddOrEven === 'Pari' || oddOrEven === 'Dispari') {
+  number = prompt('Scegli un numero da 1 a 5')
+} else {
+  alert("Segui le istruzioni")
+}
+
+if (number === 1 || number === 2 || number === 3 || number === 4 || number === 5) {
+
+} else {
+  alert('Segui le istruzioni')
+}
